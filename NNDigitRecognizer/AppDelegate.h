12fb9c55e243fd7de7aee28069c5a7f601db6d27
Yasuhiro Mitsuno <s14mm336@mail.saitama-u.ac.jp>
2014-10-19 17:52:11 +0900

    Initial Commit

---
+++ NNDigitRecognizer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NNDigitRecognizer
+//
+//  Created by yasu on 2014/10/19.
+//  Copyright (c) 2014年 yasu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NNDigitRecognizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NNDigitRecognizer
+//
+//  Created by yasu on 2014/10/19.
+//  Copyright (c) 2014年 yasu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
